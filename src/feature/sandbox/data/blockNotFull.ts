d6753430c6b5200645e32284ad503ff8d016728c
SeanardK <seanard23@gmail.com>
2025-10-11 20:36:34 +0700

    feat(stair): add stair model, texture, rotation handling, and stair list display
    - Add 3D model for stair
    - Add texture for stair model
    - Implement rotation handling for stairs
    - Display stair list in the UI

--- src/feature/sandbox/data/blockNotFull.ts
+++ src/feature/sandbox/data/blockNotFull.ts
@@ -1,1 +1,17 @@
 export const listBlockIdWithHeight90Percent = ["208", "60", "9", "120"];
+export const listStairId = [
+  "67",
+  "53",
+  "108",
+  "109",
+  "114",
+  "128",
+  "134",
+  "135",
+  "136",
+  "137",
+  "156",
+  "163",
+  "180",
+  "203",
+];
